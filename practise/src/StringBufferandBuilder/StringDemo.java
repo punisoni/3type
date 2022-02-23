3dca100d54fdb410f5890c61ba4493b45a4bc71c
punisoni <99087479+punisoni@users.noreply.github.com>
2022-02-23 15:00:26 +0530

    Add files via upload

---
+++ practise/src/StringBufferandBuilder/StringDemo.java
@@ -0,0 +1,19 @@
+package StringBufferandBuilder;
+//String Buffer//
+public class StringDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		StringBuffer s= new StringBuffer("Hello unique ");
+		s.append("Java Programming");
+		System.out.println(s);
+		
+
+	}
+
+
+		
+	}
+
+
---
+++ practise/src/unique/positive.java
@@ -0,0 +1,38 @@
+package unique;
+import java. util.Scanner;
+
+public class positive 
+{
+
+	public static void main(String[] args) 
+	{
+	        int P = 1,N=0000;
+		    Scanner s=new Scanner(System.in);
+			System.out.println("enter your Report");
+			int Report=s.nextInt();
+		try
+			{
+			if(P<=1  && N<=0)
+			{
+				System.out.println("you are positive Report");
+				
+			}
+			else
+			{   
+				
+				
+				System.out.println("you are Negative Report");
+				
+			
+			}
+			}
+			catch(Exception e)
+			{
+			System.out.println("e")	;
+			}
+			}
+		}
+
+
+
+	
